206aef075b299bddc89596179c16a8a5e9b20bd6
Tyldar <tyldar@hotmail.fr>
2017-08-15 12:47:24 +0200

    Example createService with launchConfig

--- src/Helpers.php
+++ src/Helpers.php
@@ -2,7 +2,10 @@
 namespace Tyldar\Rancher;
 
 use Tyldar\Rancher\Resources\Containers;
+use Tyldar\Rancher\Resources\Services;
+
 use Tyldar\Rancher\Models\Container;
+use Tyldar\Rancher\Models\Service;
 
 class Helpers
 {
@@ -38,5 +41,39 @@ class Helpers
         ];
 
         return $containers->create($cont);
+    }
+
+    public function createService($name, $image, $ports)
+    {
+        $services = new Services($this->client);
+
+        $cont = new Container();
+        $cont->name = $name;
+
+        //Eg: docker:nginx
+        $cont->imageUuid = $image;
+
+        //Eg: [30001:80/tcp]
+        $cont->ports = $ports;
+
+        $cont->secrets = [];
+
+        //Why ?
+        $cont->labels = [
+            "io.label.test"=>"test",
+            "io.label.test2"=>"test2"
+        ];
+        $cont->environment = [
+            "test"=>"test"
+        ];
+
+        $service = new Service();
+        $service->name = $name;
+        $service->imageUuid = $image;
+        $service->launchConfig = $cont;
+        $service->stackId = "1st6";
+        $service->secondaryLaunchConfigs = [];
+
+        return $services->create($service);
     }
 };
